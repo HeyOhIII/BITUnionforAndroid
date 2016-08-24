2f7b89eaa37b9e66e39ad2563fa9abcd5d17e14d
huolangzc <bit_zc@163.com>
2016-08-24 10:10:36 +0800

    新增工具类ToastUtil

---
+++ app/src/main/java/com/example/bitunion/util/ToastUtil.java
@@ -0,0 +1,34 @@
+package com.example.bitunion.util;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.bitunion.BUApp;
+
+/**
+ * Created by huolangzc on 2016/8/23.
+ */
+public class ToastUtil {
+    private static Toast toast;
+
+    public static void showToast(@NonNull String message) {
+        if (BUApp.getInstance() != null) {
+            if (toast == null)
+                toast = Toast.makeText(BUApp.getInstance(), "", Toast.LENGTH_SHORT);
+            toast.setText(message);
+            toast.show();
+        }
+        Log.i("Toast", message);
+    }
+
+    public static void showToast(int res) {
+        if (BUApp.getInstance() != null) {
+            if (toast == null)
+                toast = Toast.makeText(BUApp.getInstance(), "", Toast.LENGTH_SHORT);
+            toast.setText(res);
+            toast.show();
+        }
+        Log.i("Toast", BUApp.getInstance().getString(res));
+    }
+}
