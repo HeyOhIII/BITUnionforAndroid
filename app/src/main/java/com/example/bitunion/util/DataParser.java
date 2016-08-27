fa0a150d950df045ada03c3161b26d9cd5589067
huolangzc <bit_zc@163.com>
2016-08-27 09:16:30 +0800

    新增工具类DataParser&Utils

---
+++ app/src/main/java/com/example/bitunion/util/DataParser.java
@@ -0,0 +1,53 @@
+package com.example.bitunion.util;
+
+import android.util.Log;
+
+import com.example.bitunion.model.BUPost;
+import com.example.bitunion.model.BUThread;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class DataParser {
+    private static final String TAG = DataParser.class.getSimpleName();
+
+    public static ArrayList<BUThread> parseThreadlist(JSONObject rawJson) {
+        JSONArray array = rawJson.optJSONArray("threadlist");
+        if (array == null)
+            return null;
+        ArrayList<BUThread> list = new ArrayList<BUThread>();
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject threadObj = array.optJSONObject(i);
+            if (threadObj != null)
+                try {
+                    list.add(new BUThread(threadObj));
+                } catch (JSONException e) {
+                    Log.w(TAG, "Failed parsing thread >> " + threadObj.toString(), e);
+                    continue;
+                }
+        }
+        return list;
+    }
+
+    public static ArrayList<BUPost> parsePostlist(JSONObject rawJson) {
+        JSONArray array = rawJson.optJSONArray("postlist");
+        if (array == null)
+            return null;
+        ArrayList<BUPost> list = new ArrayList<BUPost>();
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject postObj = array.optJSONObject(i);
+            if (postObj != null)
+                try {
+                    list.add(new BUPost(postObj));
+                } catch (JSONException e) {
+                    Log.w(TAG, "Failed parsing post >> " + postObj.toString(), e);
+                    continue;
+                }
+        }
+        return list;
+    }
+}
---
+++ app/src/main/java/com/example/bitunion/util/Utils.java
@@ -0,0 +1,242 @@
+package com.example.bitunion.util;
+
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DecimalFormat;
+
+public class Utils {
+    public static final int EXIT_WAIT_TIME = 2000;
+
+    public static final String QUOTE_HEAD = "<br><br><center><table[^>]+><tr><td>&nbsp;&nbsp;引用(?:\\[<a href='[\\w\\.&\\?=]+?'>查看原帖</a>])*?.</td></tr><tr><td><table.{101,102}bgcolor='ALTBG2'>";
+    public static final String QUOTE_TAIL = "</td></tr></table></td></tr></table></center><br>";
+    public static final String QUOTE_REGEX = QUOTE_HEAD
+            + "(((?!<br><br><center><table border=)[\\w\\W])*?)" + QUOTE_TAIL;
+
+    public static boolean isBitUnionUrl(String url) {
+        Uri uri = Uri.parse(url);
+        return uri.getHost().matches("^(www|v6|kiss|out).bitunion.org$");
+    }
+
+    public static String replaceHtmlChar(String str) {
+        String htmlstring = str;
+        htmlstring = htmlstring.replace("&amp;", "&");
+        htmlstring = htmlstring.replace("&nbsp;", " ");
+        htmlstring = htmlstring.replace("&lt;", "<");
+        htmlstring = htmlstring.replace("&gt;", ">");
+        return htmlstring;
+    }
+
+    public static String hashImgUrl(String imgUrl) throws NoSuchAlgorithmException {
+        String imgKey = null;
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.reset();
+        m.update(imgUrl.getBytes());
+        byte[] digest = m.digest();
+        BigInteger bigInt = new BigInteger(1, digest);
+        imgKey = bigInt.toString(16);
+        while (imgKey.length() < 32)
+            imgKey = "0" + imgKey;
+        return imgKey;
+    }
+
+    /**
+     * Convert file size to human friendly format (e.g. 254K, 4.6M)
+     * @param size File size in bytes
+     * @return Human readable file size
+     */
+    public static String getReadableFileSize(long size) {
+        int unit = 1; // Byte
+        while (size >= unit<<10)
+            unit = unit << 10;
+        String sizeStr = "";
+        if (size/unit >= 100)
+            sizeStr = Long.toString(size/unit);
+        else
+            sizeStr = new DecimalFormat("##.#").format((double)size/unit);
+        String unitStr = "B";
+        switch (unit) {
+            case 1: unitStr = "B"; break;
+            case 1<<10: unitStr = "K"; break;
+            case 1<<20: unitStr = "M"; break;
+            default: unitStr = "G"; break;
+        }
+        return sizeStr + unitStr;
+    }
+
+    /**
+     * Get readable string of file size (e.g. 24B, 245k, 4.5M)
+     *
+     * @return display string
+     */
+    public static String getFileSizeString(long fileSize) {
+        // TODO
+        return null;
+    }
+
+    public static String readTextFromInputStream(InputStream is) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] data = new byte[1024];
+        int len;
+        String result = null;
+        if (is == null)
+            return null;
+        while ((len = is.read(data)) != -1) {
+            outputStream.write(data, 0, len);
+        }
+        result = new String(outputStream.toByteArray());
+        return result;
+    }
+
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri     The Uri to query.
+     * @author paulburke
+     * @see <a href="http://stackoverflow.com/questions/19834842/android-gallery-on-kitkat-returns-different-uri-for-intent-action-get-content">Android Gallery on KitKat returns different Uri for Intent.ACTION_GET_CONTENT</a>
+     */
+    @SuppressLint("NewApi")
+    public static String getRealPathFromUri(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+}
